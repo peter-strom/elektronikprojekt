30402c2cbcbfff840e3e81e3c30bea3eea4bd45a
Peter Strömblad <peter@consol.se>
2022-02-21 11:09:42 +0100

    testing rp pico

---
+++ rp-pico-test/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN,OUTPUT);
+}
+
+void loop() {
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1000);
+digitalWrite(LED_BUILTIN,LOW);
+delay(1000);
+
+
+  // put your main code here, to run repeatedly:
+}
