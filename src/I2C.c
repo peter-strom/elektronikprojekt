dc8d74350229ebf4bf0af249a6d2c3156fcaf50d
peter_desktop <peter@DESKTOP-UHP7NO9>
2022-04-06 15:56:22 +0200

    test

--- src/I2C.c
+++ src/I2C.c
@@ -10,6 +10,49 @@ void init_i2c(void)
     i2c_init(i2c, 400 * 1000); //400khz
 }
 
+static void mpu6050_reset() {
+    // Two byte reset. First byte register, second byte data
+    // There are a load more options to set up the device in different ways that could be added here
+    uint8_t buf[] = {0x6B, 0x00};
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+}
+
+void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+    // For this particular device, we send the device the register we want to read
+    // first, then subsequently read from the device. The register is auto incrementing
+    // so we don't need to keep sending the register we want, just the first.
+
+    uint8_t buffer[6];
+
+    // Start reading acceleration registers from register 0x3B for 6 bytes
+    uint8_t val = 0x3B;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
+
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+
+    // Now gyro data from reg 0x43 for 6 bytes
+    // The register is auto incrementing on each read
+    val = 0x43;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+    }
+
+    // Now temperature from reg 0x41 for 2 bytes
+    // The register is auto incrementing on each read
+    val = 0x41;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 2, false);  // False - finished with bus
+
+    *temp = buffer[0] << 8 | buffer[1];
+}
+
+
 
  
 // Write 1 byte to the specified register
--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main(void)
 
     uint8_t data = 5;
     //reg_read(i2c_unit,i2c_device_adress,device_register,*data_pointer, no_bytes)
-    reg_read(i2c,I2C_ADDRESS,0xC0,&data,1);
+    //reg_read(i2c,I2C_ADDRESS,0xC0,&data,1);
     while(true)
     {
         printf("data: %d\n",data);
