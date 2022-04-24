f47540e5f0e523e25ba7cb62a8a2bf7be9fa5d25
peter_desktop <peter@DESKTOP-UHP7NO9>
2022-04-24 13:45:52 +0200

    cleaned up the PID-struct

--- src/PID.c
+++ src/PID.c
@@ -1,116 +1,122 @@
 #include "PID.h"
 
-
-double sanitize_input(PID* self, double sensor_input); //kontrollera insignal från sensor
-void map_input(PID* self); //Mappningsfunktion
-void sanitize_output(PID* self); // Kontrollerar utsignalens värde, jämför med min- och max.
+double sanitize_input(PID* self, double sensor_input);
+void map_input(PID* self);
+void sanitize_output(PID* self);
 void regulate(PID* self); 
 void print(PID* self); 
 
 /**
- * @brief
+ * @brief Initialize an instance of a PID-structure.
  * 
- * @param target in degrees
- * @param Kp 
- * @param Ki 
- * @param Kd 
- * @param output_min in degrees 0 
- * @param output_max in degrees 180
- * @return PID 
+ * @param target Target or setpoint in degrees (90).
+ * @param Kp Proportional gain constant.
+ * @param Ki Integral gain constant.
+ * @param Kd Derivative gain constant.
+ * @param output_min min steering angle in degrees
+ * @param output_max max steering angle in degrees
+ * @return PID instance of a PID-structure
  */
 PID new_PID(const double target, const double Kp, const double Ki,
 	const double Kd, const double output_min, const double output_max)
 {
     PID self;
-    self.target = target; // 90 grader
-	self.output_min = output_min; // 0 grader
-	self.output_max = output_max; // 180 grader
+    self.target = target;
+	self.output_min = output_min; 
+	self.output_max = output_max; 
 	self.Kp = Kp;
 	self.Ki = Ki;
 	self.Kd = Kd;
 	self.sensor_max = 1023.0; 
 	self.sensor_min = 0.0;
 
-	self.actual_value = 0; // Ärvärde, det värde vi faktiskt har.
-	self.output = 0; // Regulatorns utsignal, anpassas efter bör- och ärvärde.
-	self.current_error = 0; // Aktuellt fel.
-	self.last_error = 0; // Föregående fel.
-    self.integral = 0; // Integralen av aktuellt fel (ökar/minskar i takt med aktuellt fel).
-	self.derivative = 0; // Derivatan av felet (aktuellt fel - föregående fel).
+	self.actual_value = 0; 
+	self.output = 0; 
+	self.current_error = 0; 
+	self.previous_error = 0; 
+    self.integral = 0; 
+	self.derivative = 0;
 
 	self.left_sensor_input = 0x00;
 	self.right_sensor_input = 0x00;
 	self.mapped_right_sensor_input = 0x00;
 	self.mapped_left_sensor_input = 0x00;
-
-
-
     return self;
 }
 
-
+/**
+ * @brief Prevents output data from subceed/exceed the min/max allowed value.
+ * 
+ * @param self Pointer address to the PID instance.
+ * @param sensor_input Non sanitized sensor value.
+ * @return double - Sanitized sensor value.
+ */
 double sanitize_input(PID* self, double sensor_input)
 {
 	if(sensor_input > self->sensor_max)
 	{
 		return self->sensor_max;
 	}
-	else if (sensor_input < self->sensor_min)
+	else if(sensor_input < self->sensor_min)
 	{
 		return self->sensor_min;
 	}
 	return sensor_input;
 }
 
-	// Sparat mappat värde av vänster insignal (från 0 - 1023 ned till 0 - 180):
+/**
+ * @brief converts the sensor distance values to servo motor degrees.
+ * 
+ * @param self Pointer address to the PID instance
+ */
 void map_input(PID* self)
 {
 	self->mapped_left_sensor_input = self->left_sensor_input / self->sensor_max * self->target;
 	self->mapped_right_sensor_input = self->right_sensor_input / self->sensor_max * self->target;
 }
 
-/***************************************************************************************************
-* Metoden check_output används för att kontrollera så att aktuell utsignal inte överstiger
-* satt maximumvärde eller understiger angivet minimumvärde. Om användaren inte har angivit några
-* värden eller om samma värden på min- och max har angivits, så görs ingen kontroll. Annars
-* justeras utsignal efter behov.
-***************************************************************************************************/
+/**
+ * @brief prevents output data from subceed/exceed the min/max allowed value.
+ * 
+ * @param self Pointer address to the PID instance
+ */
 void sanitize_output(PID* self)
 {
-	if (self->output_min == self->output_max)
-		return;
-	if (self->output > self->output_max)
+	if(self->output > self->output_max)
+	{
 		self->output = self->output_max;
-	else if (self->output < self->output_min)
+	}
+	else if(self->output < self->output_min)
+	{
 		self->output = self->output_min;
+	}
 }
 
-
-// Genomför reglering.
-/***************************************************************************************************
-* Metoden regulate används för att genomföra en reglering på en PID-regulator. Aktuellt fel
-* beräknas som differensen mellan aktuellt bör- och ärvärde. Integralen av felet uppdateras genom
-* att addera aktuellt fel. Derivatan av felete beräknas som differensen mellan aktuellt fel och
-* föregående fel. Därmed gäller att om nuvarande fel är större än föregående fel, så ökar felet.
-* D-delen av regulatorn kommer då medföra ökar reglering, så att felet minskar. Utsignalen
-* uppdateras genom att addera börvärdet med respektive del av regulatorn (Kp * aktuellt fel
-* för P-delen, Ki * integralen för I-delen samt Kd * derivatan för D-delen). Uppdated utsignal
-* kontrolleras via anrop av metoden check_output. Nuvarande fel sparas sedan som föregående fel
-* inför nästa reglering (behövs för beräkning av derivatan).
-***************************************************************************************************/
+/**
+ * @brief Where the PID control regulation happens
+ * 
+ * @param self Pointer address to the PID instance
+ */
 void regulate(PID* self)
 {
 	self->current_error = self->target - self->actual_value;
 	self->integral += self->current_error;
-	self->derivative = self->current_error - self->last_error;
+	self->derivative = self->current_error - self->previous_error;
 	self->output = self->target + self->Kp * self->current_error +
 		self->Ki * self->integral + self->Kd * self->derivative;
 	sanitize_output(self);
-	self->last_error = self->current_error;
+	self->previous_error = self->current_error;
 }
 
-
-double PID_regulate(PID* self, const double new_left_sensor_input, const double new_right_sensor_input)
+/**
+ * @brief Calculate a servo steering value from two sensors.
+ * 
+ * @param self address to the PID instance
+ * @param new_left_sensor_input value from the left sensor
+ * @param new_right_sensor_input value from the right sensor
+ * @return double 
+ */
+double PID_get_servo_value_from_sensors(PID* self, const double new_left_sensor_input, const double new_right_sensor_input)
 {
 	self->left_sensor_input = sanitize_input(self, new_left_sensor_input);
 	self->right_sensor_input = sanitize_input(self, new_right_sensor_input);
@@ -121,12 +127,11 @@ double PID_regulate(PID* self, const double new_left_sensor_input, const double 
 	return self->output;
 }
 
-// Skriver ut PID-regulatorns aktuella parametrar.
-
-/***************************************************************************************************
-* Metoden print används för att skriva ut aktuella parametrar gällande en PID-regulator. Här
-* sker utskrift av börvärdet, ärvärdet, aktuell utsignal, aktuellt fel samt föregående fel.
-***************************************************************************************************/
+/**
+ * @brief Prints the values of the structure members from chosen PID instance. 
+ * 
+ * @param self Pointer address to the PID instance you want to examine.
+ */
 void print(PID* self)
 {
 	printf("----------------------------------------------------------------------------\n");
@@ -134,9 +139,9 @@ void print(PID* self)
 	printf("Actual value: %f\n", self->actual_value);
 	printf("Output: %f\n", self->output);
 	printf("Current error: %f\n", self->current_error);
-	printf("Last error: %f\n", self->last_error);
+	printf("previous error: %f\n", self->previous_error);
 	printf("----------------------------------------------------------------------------\n\n");
-		printf("Left input: %f\n", self->left_sensor_input);
+	printf("Left input: %f\n", self->left_sensor_input);
 	printf("Right input: %f\n", self->right_sensor_input);
 	printf("Mapped left input: %f\n", self->mapped_left_sensor_input);
 	printf("Mapped right input: %f\n", self->mapped_right_sensor_input);
@@ -153,6 +158,4 @@ void print(PID* self)
 	{
 		printf("unit driving: streight ahead\n\n");
 	}
-
-
 }
--- src/PID.h
+++ src/PID.h
@@ -2,42 +2,33 @@
 #define PID_H_
 #include "header.h"
 
-
 typedef struct PID 
 {
-	double target; // Börvärde, det värde vi strävar efter.
-	double output_min; // Utsignalens minimumvärde.
-	double output_max; // Utsignalen maximumvärde.
-	double Kp; // Förstärkningsfaktor för proportionerlig del (P-delen).
-	double Ki; // Förstärkningsfaktor för integrerande del (I-delen).
-	double Kd; // Förstärkningsfaktor för deriverande del (D-delen).
+	double target; 
+	double output_min; 
+	double output_max; 
+	double Kp; 
+	double Ki; 
+	double Kd; 
 	double sensor_max; 
 	double sensor_min;
 	
-	double actual_value; // Ärvärde, det värde vi faktiskt har. Ärvärde = 90 + vänster - höger
-	double output; // Regulatorns utsignal, anpassas efter bör- och ärvärde.
-	double current_error; // Aktuellt fel.
-	double last_error; // Föregående fel.
-	double integral; // Integralen av aktuellt fel (ökar/minskar i takt med aktuellt fel).
-	double derivative; // Derivatan av felet (aktuellt fel - föregående fel).
+	double actual_value;
+	double output;
+	double current_error; 
+	double previous_error; 
+	double integral; 
+	double derivative;
 
 	double left_sensor_input;
 	double right_sensor_input;
 	double mapped_right_sensor_input;
 	double mapped_left_sensor_input;
-
-
-
-
 } PID;
-
 
 PID new_PID(const double target, const double Kp, const double Ki, const double Kd,
 		const double output_min, const double output_max);
 
-double PID_regulate(PID* self, const double new_left_sensor_input, const double new_right_sensor_input);
-
-
-
+double PID_get_servo_value_from_sensors(PID* self, const double new_left_sensor_input, const double new_right_sensor_input);
 
 #endif /* PID_H_ */
