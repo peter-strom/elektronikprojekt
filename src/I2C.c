ba47cdfbf94405d8cafd97f9dc033417714cefc4
Peter Strömblad <peter@kungariket.se>
2022-04-29 10:54:53 +0200

    comments and cleanup

--- src/I2C.c
+++ src/I2C.c
@@ -8,19 +8,35 @@
  */
 void init_i2c(void)
 {
-    i2c_init(i2c_default, 400 * 1000);                         
+    i2c_init(i2c_default, 400 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     // Make the I2C pins available to picotool ??
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
 }
 
+/**
+ * @brief Write 8-bits to a register on a specific i2c device
+ *
+ * @param i2c_addr The physical address of the i2c devie (8-bit)
+ * @param reg The register on the i2c device to write to.
+ * @param val The value to write
+ * @return bool
+ *
+ */
 bool i2c_write_uint8_to_reg(const uint8_t i2c_addr, const uint8_t reg, const uint8_t val)
 {
     uint8_t buf[] = {reg, val};
     return i2c_write_blocking(i2c_default, i2c_addr, buf, 2, false);
 }
 
+/**
+ * @brief Read 8-bits from a register on a specific i2c device
+ *
+ * @param i2c_addr The physical address of the i2c devie (8-bit)
+ * @param reg The register on the i2c device to read from.
+ * @return uint8_t
+ */
 uint8_t i2c_read_uint8_from_reg(const uint8_t i2c_addr, const uint8_t reg)
 {
     uint8_t ret;
@@ -29,6 +45,13 @@ uint8_t i2c_read_uint8_from_reg(const uint8_t i2c_addr, const uint8_t reg)
     return ret;
 }
 
+/**
+ * @brief Read 16-bits from a register on a specific i2c device
+ *
+ * @param i2c_addr The physical address of the i2c devie (8-bit)
+ * @param reg The register on the i2c device to read from.
+ * @return uint8_t
+ */
 uint16_t i2c_read_uint16_from_reg(const uint8_t i2c_addr, const uint8_t reg)
 {
     uint8_t ret[2];
@@ -37,6 +60,13 @@ uint16_t i2c_read_uint16_from_reg(const uint8_t i2c_addr, const uint8_t reg)
     return ((ret[0] << 8) | (ret[1]));
 }
 
+/**
+ * @brief Read 32-bits from a register on a specific i2c device
+ *
+ * @param i2c_addr The physical address of the i2c devie (8-bit)
+ * @param reg The register on the i2c device to read from.
+ * @return uint8_t
+ */
 uint32_t i2c_read_uint32_from_reg(const uint8_t i2c_addr, const uint8_t reg)
 {
     uint8_t ret[4];
@@ -45,9 +75,16 @@ uint32_t i2c_read_uint32_from_reg(const uint8_t i2c_addr, const uint8_t reg)
     return ((ret[0] << 24) | (ret[1] << 16) | (ret[2] << 8) | (ret[3]));
 }
 
+/**
+ * @brief Read no_of_bytes from a register on a specific i2c device
+ *
+ * @param i2c_addr The physical address of the i2c devie (8-bit)
+ * @param reg The register on the i2c device to read from.
+ * @param no_of_bytes Lengt of data in bytes to receive
+ * @param ret_val Pointer to buffer to receive data
+ */
 void i2c_read_bytes_from_reg(const uint8_t i2c_addr, const uint8_t reg, uint8_t no_of_bytes, uint8_t *ret_val)
 {
-
     i2c_write_blocking(i2c_default, i2c_addr, &reg, 1, true);
     i2c_read_blocking(i2c_default, i2c_addr, ret_val, no_of_bytes, false);
 }
